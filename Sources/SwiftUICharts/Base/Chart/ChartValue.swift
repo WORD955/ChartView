4f947ead63653bc29819edacc14121356cc03f16
conversun <conversun@gmail.com>
2021-12-08 19:07:00 +0800

    public it

--- Sources/SwiftUICharts/Base/Chart/ChartValue.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartValue.swift
@@ -2,6 +2,6 @@ import SwiftUI
 
 /// Representation of a single data point in a chart that is being observed
 public class ChartValue: ObservableObject {
-    @Published var currentValue: Double = 0
-    @Published var interactionInProgress: Bool = false
+    @Published public var currentValue: Double = 0
+    @Published public var interactionInProgress: Bool = false
 }
